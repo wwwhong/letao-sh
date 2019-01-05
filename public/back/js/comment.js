d24b1a787c63e7227afc7e864f0fb0f6bae39158
wwwhong <ride111@qq.com>
2019-01-05 10:12:18 +0800

    index首页完成

--- public/back/js/comment.js
+++ public/back/js/comment.js
@@ -27,4 +27,40 @@ $(function(){
     $(document).ajaxStop(function(){
         NProgress.done();
     });
-})
+});
+
+// 入口函数, 等待当前的 dom 结构, 加载完成后, 执行
+$(function(){
+    //公共的功能
+    //1.左侧二级菜单切换
+    $(".lt_aside .category").on("click",function(){
+        $(this).next().stop().slideToggle();
+    });
+
+    //2.左侧栏切换功能
+    $('.topBar .icon_menu').on('click',function(){
+        $('.lt_aside').toggleClass('hid');
+        $(".lt_main").toggleClass('hideMenu');
+        $(".lt_main .topBar").toggleClass('hideMenu');
+    });
+
+    //3.退出功能
+    // (1)显示模态框
+    $('.lt_main .icon_logout').on('click',function(){
+        $('#myModal').modal("show");
+    });
+    //(2)点击退出按钮,发送ajax请求,让后台删除sessionId
+    $("#logoutBtn").on("click",function(){
+        $.ajax({
+            data:"get",
+            url:"/employee/employeeLogout",
+            dataType:'json',
+            success:function(info){
+                // console.log ( info );
+                if(info.success ){//返回数据 success:true 成功 跳到首页
+                    location.href="login.html";
+                }
+            }
+        })
+    })
+});
